d56a602f50ea50267c33433474718f321319d6e2
Gokay Kucuk <gokay@feingeist.io>
2017-03-27 14:50:15 +0200

    Added methods for getting item by id and uploading png files

--- lib/confluence/api/client.rb
+++ lib/confluence/api/client.rb
@@ -5,7 +5,7 @@ require 'faraday'
 module Confluence
   module Api
     class Client
-      attr_accessor :user, :pass, :url, :conn
+      attr_accessor :user, :pass, :url, :conn, :multiconn
       def initialize(user, pass, url)
         self.user = user
         self.pass = pass
@@ -16,12 +16,38 @@ module Confluence
           faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
           faraday.basic_auth(self.user, self.pass)
         end
+        self.multiconn = Faraday.new(url: url ) do |faraday|
+          faraday.request  :multipart             # form-encode POST params
+          # faraday.response :logger                  # log requests to STDOUT
+          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+          faraday.basic_auth(self.user, self.pass)
+        end
+
+
       end
 
       def get(params)
         response = conn.get('/wiki/rest/api/content', params)
         JSON.parse(response.body)['results']
       end
+
+      def get_by_id(id)
+        response = conn.get('/wiki/rest/api/content/' + id)
+        JSON.parse(response.body)
+      end
+
+      def upload_png(id,file_path)
+        puts file_path
+        response = multiconn.post do |req|
+          req.url '/wiki/rest/api/content/'+id+'/child/attachment'
+          req.headers['X-Atlassian-Token']= 'nocheck'
+          req.body= {file: Faraday::UploadIO.new(File.open(file_path), 'image/png') }
+        end
+        JSON.parse(response.body)
+      end
+
+
+      #def update_png
 
       def create(params)
         response = conn.post do |req|
